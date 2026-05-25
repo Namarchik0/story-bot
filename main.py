e2c9edbb660fafa975b9e564b4314717a438448e
Namarchik0 <namar0223kizil@gmail.com>
2026-05-25 16:24:56 +0300

    fix render port

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+def keep_alive():
+    t = Thread(target=run_web)
+    t.start()
+
 import asyncio
 import logging
 
